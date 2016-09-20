0e77baec9f24edff38bf0c7733f4e62b74831e98
Tetiana Khotiaintseva <tetiana.khotiaintseva@ardigen.com>
2016-09-20 16:32:24 +0200

    Add provision script

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,30 +1,10 @@
 echo "Installing system packages..."
-sudo apt-get update 1>/dev/null
-sudo apt-get install -y \
-	vim \
-	git \
-	software-properties-common \
-	python-pip \
-	python-dev \
-	python-software-properties \
-	build-essential \
-	python-pyrex \
-	zlib1g-dev 
-if ! type -p java; then
-	echo
-	echo "Installing Java..."
-	echo "Adding ppa:nilarimogard/webupd8 repo..."
-	sudo add-apt-repository -y ppa:nilarimogard/webupd8
-	sudo apt-get update 1>/dev/null
-	sudo apt-get install -y launchpad-getkeys
-	sudo launchpad-getkeys
-	echo
-	echo "Adding ppa:webupd8team/java repo..."
-	sudo add-apt-repository -y ppa:webupd8team/java
-	sudo apt-get update 1>/dev/null
-	wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_java8.sh && bash equip_java8.sh
-	sudo apt-get install -y oracle-java8-installer --force-yes 
-fi;
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get -y update
+sudo apt-get install -y git python-pip python-dev apt-get install software-properties-common python-software-properties build-essential python-pyrex zlib1g-dev 
+sudo add-apt-repository -y ppa:webupd8team/java 
+sudo apt-get -y update
+sudo apt-get install -y oracle-java8-installer
 
 echo "Installing pyhton pachakges..."
 pip install -U pip
@@ -32,15 +12,13 @@ pip install Cython
 pip install toolz
 pip install umis
 
-
+chmod -R 777 /usr/local  # to avoid problems with bcbio installation
+exit
 echo
 echo "Installing bcbio..."
-chmod -R 777 /usr/local  # to avoid problems with bcbio installation
-cd /vagrant
+python /vagrant/scripts/bcbio_nextgen_install.py /usr/local/share/bcbio --tooldir=/usr/local --nodata
 
-su vagrant -c "python scripts/bcbio_nextgen_install.py /usr/local/share/bcbio --tooldir=/usr/local --nodata"
-
-su vagrant -c "bcbio_nextgen.py upgrade --tools --toolplus gatk=/vagrant/GenomeAnalysisTK.jar --tooldir=/usr/local 1>/dev/null"
+bcbio_nextgen.py upgrade --tools --toolplus gatk=/vagrant/GenomeAnalysisTK.jar --tooldir=/usr/local
 
 echo
 echo "Installing go lang..."
