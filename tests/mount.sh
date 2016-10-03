cb47ee6f302b411d8964293de8e7382a6cda2cac
Tetiana Khotiaintseva <tetiana.khotiaintseva@ardigen.com>
2016-10-03 14:43:17 +0200

    Fix scripts for running tests

--- tests/mount.sh
+++ tests/mount.sh
@@ -2,16 +2,16 @@
 
 
 _BUCKET="testbucket"
-checkmounted(){ grep -qs "$1" "/proc/mounts";}
+checkmounted(){ grep -s "$1" "/proc/mounts";}
 
 
 if checkmounted "$_BUCKET"; then
 	echo "S3 bucket is mounted."
 else 
 	echo "Mounting the bucket..."
-	cmd = "goofys --endpoint s3.eu-central-1.amazonaws.com --sse testbcbio /mnt/$_BUCKET"
-    echo cmd
-    cmd
+	cmd="goofys --endpoint s3.eu-central-1.amazonaws.com --sse testbcbio /mnt/$_BUCKET"
+	echo $cmd
+	$cmd
 	if checkmounted "$_BUCKET"; then
 		echo "Successfully mounted S3 bucket."
 	else
@@ -21,9 +21,10 @@ else
 	fi
 fi
 
-echo "BCBIO workdir is now $BCBIO_WORKDIR"
 export BCBIO_WORKDIR=/mnt/testbucket/testworkdir
+echo "BCBIO workdir is now $BCBIO_WORKDIR"
 
 deactivate () {
 	unset BCBIO_WORKDIR
+	echo "Bcbio workdir is no longer $BCBIO_WORKDIR"
 }
--- tests/test.sh
+++ tests/test.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+set -e
+
+readlinkf(){ perl -MCwd -e 'print Cwd::abs_path shift' $1;}
 
 BCBIO_DIR=$(dirname "$(readlinkf `which bcbio_nextgen.py`)")
 TEST_CLASS="test_automated_analysis.py:AutomatedAnalysisTest"
 
-set -e
 
 echo 
 cmd="$BCBIO_DIR/nosetests -v -s $TEST_CLASS.$1"
